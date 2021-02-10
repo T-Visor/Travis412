7a57b7ec5be5a257b609efa203dcbaa8c903e989
T-Visor <turkimbro@yahoo.com>
2021-02-09 23:40:53 -0500

    added a testSubtract() method to SimpleCalculatorTest. This results in a compilation error since a subtract() method doesn't exist in SimpleCalculator. This should result in a build failure for the Travis Continuous Integration tool.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -5,10 +5,19 @@ import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class SimpleCalculatorTest {
+public class SimpleCalculatorTest 
+{
 	@Test
-	public void testAdd() {
+	public void testAdd() 
+	{
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2); // changed from 0 back to 2
+	}
+	
+	@Test
+	public void testSubtract()
+	{
+		SimpleCalculator calculator = new SimpleCalculator();
+		assertEquals(calculator.subtract(5, 3), 2);
 	}
 }
