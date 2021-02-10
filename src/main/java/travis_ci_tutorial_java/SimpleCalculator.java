1422acb05735ff6883eb1b1959dcefb686d897b1
T-Visor <turkimbro@yahoo.com>
2021-02-09 23:51:07 -0500

    added the subtract() method to SimpleCalculator to get rid of the compilation error from the SimpleCalculatorTest class.

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,22 @@
 package travis_ci_tutorial_java;
 
-public class SimpleCalculator {
-	public int add(int a, int b) {
+public class SimpleCalculator 
+{
+	public int add(int a, int b) 
+	{
 		return a + b;
+	}
+	
+	/**
+	 * Subtract the second argument from the first.
+	 * 
+	 * @param first the first operand
+	 * @param second the second operand
+	 * 
+	 * @return the difference between the two operands
+	 */
+	public int subtract(int first, int second)
+	{
+		return first - second;
 	}
 }
